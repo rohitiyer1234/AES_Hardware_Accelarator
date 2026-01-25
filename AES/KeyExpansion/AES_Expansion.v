efe13061ba639ab27f044621992371e382b5546e
Rohit Iyer <143709967+rohitiyer1234@users.noreply.github.com>
2026-01-25 22:43:48 +0530

    Update AES_Expansion.v

--- AES/KeyExpansion/AES_Expansion.v
+++ AES/KeyExpansion/AES_Expansion.v
@@ -1,70 +1,83 @@
-module AES_Expansion(
-    input reset, clk, start,
-    input [127:0] short_subkey,
-    output reg [127:0] subkey,
-    output subkey_valid
-    );
+module Aes_key_expansion_128(
+    input clk,
+    input reset,
+    input key_expansion_start,                   //Flag to indicate start of key expansion FSM
+    input [127:0] user_sub_key,     //User given key
+        //128 bit cypher key generated every round 
+              //To keep count of which round is it 0-11 , for simplification rn it is 4 
+             //output_key generated is valid only when status=1
 
+    output reg w_en,
+    output reg [3:0]  waddr,
+    output reg [127:0] wkey
 
-     // FSM states
-    localparam IDLE = 3'd0;
-    localparam LOAD_BASE_KEY = 3'd1;
-    localparam GENERATE_WORD = 3'd2;
-    localparam WRITE_WORD = 3'd3;
-    localparam DONE = 3'd4;
-
-    reg [3:0] i; //word index
+);
+    reg local_start;
+    reg [3:0] i;
     reg status;
-    reg [127:0] prev_period_suubkey;
+    reg [127:0] prev_period_key_1;     //To hold 128 bit previous round key for next round computation
+    wire [31:0] key1,key2,key3,key4;
 
-    wire [31:0] key1, key2, key3, key4;
-    Keyword_gen K1(.i({i,2'd0}),.prev_word(prev_period_key_1[31:0]),.prev_period_word(prev_period_key_1[127:96]),.current_word(key1));
-    Keyword_gen K2(.i({i,2'd1}),.prev_word(key1),.prev_period_word(prev_period_key_1[95:64]),.current_word(key2));
-    Keyword_gen K3(.i({i,2'd2}),.prev_word(key2),.prev_period_word(prev_period_key_1[63:32]),.current_word(key3));
-    Keyword_gen K4(.i({i,2'd3}),.prev_word(key3),.prev_period_word(prev_period_key_1[31:0]),.current_word(key4));
+    // didnt understand this part fully , look into it later as well
+    // W4= W0 XOR G(w3)
+    //W5 = W1 XOR W4
 
-    assign subkey = {key1, key2, key3, key4};
+    current_word_gen_128 word1(.i({i,2'd0}),.prev_word(prev_period_key_1[31:0]),.prev_period_word(prev_period_key_1[127:96]),.current_word(key1));
+    current_word_gen_128 word2(.i({i,2'd1}),.prev_word(key1),.prev_period_word(prev_period_key_1[95:64]),.current_word(key2));
+    current_word_gen_128 word3(.i({i,2'd2}),.prev_word(key2),.prev_period_word(prev_period_key_1[63:32]),.current_word(key3));
+    current_word_gen_128 word4(.i({i,2'd3}),.prev_word(key3),.prev_period_word(prev_period_key_1[31:0]),.current_word(key4));
 
-    always @ (posedge clk)
+
+    assign output_key ={key1,key2,key3,key4};    // 4 32 bit words to give 128 bit key
+
+
+
+    always @(posedge clk)
     begin
-        if (reset) begin
-            status <= IDLE;
+        if(reset) begin
             i <= 0;
-            prev_period_subkey <= 0;
-        end else begin
-            case (status)
-            IDLE: begin
-                if (start) begin
-                    status <= LOAD_BASE_KEY;
-                end
-            end
-
-            LOAD_BASE_KEY: begin
-                prev_period_subkey <= short_subkey;
-                i <= 0;
-                status <= GENERATE_WORD;
-            end
-
-            GENERATE_WORD: begin
-                if (i == 4) begin
-                    status <= DONE;
-                end else begin
-                    i <= i + 1;
-                    status <= WRITE_WORD;
-                end
-            end
-
-            WRITE_WORD: begin
-                prev_period_subkey <= subkey;
-                status <= GENERATE_WORD;
-            end
-
-            DONE: begin
-                status <= IDLE;
-            end
-
-            endcase
-
+            prev_period_key_1 <= 0;
+            status <= 0;
+            //local_start <= 0;
+            w_en   <= 0;
+            waddr  <= 0;
+            wkey   <= 0;
         end
-    end
+        else begin
+            w_en <= 0;   // defaul
+            
+            if (key_expansion_start && !status) begin
+                //local_start <= 1'b1;
+                i <= 1;
+                prev_period_key_1 <= user_sub_key;
+                status <= 1;
+
+                // write round-0 key
+                w_en   <= 1;
+                waddr  <= 0;
+                wkey   <= user_sub_key;
+            end
+
+            else if(status && (i>=1) && (i<10)) begin
+                prev_period_key_1 <= output_key;
+                i <= i+1;
+
+                // write round-i key
+                w_en   <= 1;
+                waddr  <= i;
+                wkey   <= output_key;
+            end
+
+            else if(status && i==10) begin
+                // write final round key
+                w_en   <= 1;
+                waddr  <= 10;
+                wkey   <= output_key;
+
+                i <= 0;
+                prev_period_key_1 <= 0;
+                status <= 0;
+            end
+        end
+end
 endmodule
